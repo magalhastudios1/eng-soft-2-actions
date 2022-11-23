aba5c827a68a47688b8422c76bf78a9b8d8b9301
magalhastudios1 <magalhastudios1@gmail.com>
2022-11-23 20:19:35 -0300

    add other operations

--- calculator_test.py
+++ calculator_test.py
@@ -2,3 +2,12 @@ import calculator
 
 def test_addition():
     assert calculator.addition(2, 3) == 5
+
+def test_subtraction():
+    assert calculator.subtraction(2, 3) == -1
+
+def test_multiplication():
+    assert calculator.multiplication(2, 3) == 6
+
+def test_division():
+    assert calculator.division(2, 3) == 0
